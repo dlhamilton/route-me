259c1b93d5fec94487300168c0f83761f4f36451
Dan Hamilton <hamiltondan93@gmail.com>
2022-10-06 17:25:48 +0000

    Add: create maze function to get the grid for the maze

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pprint import pprint
+
+
+path = 'p'
+wall = 'w'
+
+
+def create_maze():
+    width = 10
+    height = 10
+    maze = []
+    for h in range(0,height):
+        row = []
+        for w in range(0,width):
+            row.append(path)
+        maze.append(row)
+    pprint(maze)
+    return maze
+
+
 
 
 def show_menu():
@@ -22,3 +42,4 @@ def main():
 
 
 main()
+create_maze()
