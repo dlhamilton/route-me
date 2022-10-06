b255874a31d1ceee30b314197be3b5dba5bb8cf2
Dan Hamilton <hamiltondan93@gmail.com>
2022-10-06 22:31:16 +0000

    Add: prims algorithm to build walls and path  from starting point

--- run.py
+++ run.py
@@ -21,12 +21,13 @@ def create_maze():
     walls = []
     walls = get_starting_walls(walls,starting_maze_generation_position_h,starting_maze_generation_position_w)
     maze = set_starting_walls(maze,starting_maze_generation_position_h,starting_maze_generation_position_w)
+    make_maze_walls(maze,walls,10,10)
     return maze
 
 
 def get_starting_walls(walls, start_h, start_w):
     '''
-    This will add the walls surrounding the starting path to the walls array
+    This will add the walls surrounding the starting path to the walls list
     '''
     walls.append([start_h-1, start_w])
     walls.append([start_h, start_w-1])
@@ -82,6 +83,185 @@ def draw_maze(maze):
             print(f"{maze[h][w]} ",end="")
         print()
 
+
+def make_maze_walls(maze,walls,height,width):
+    '''
+    Prims Algorithm 
+    While there are walls in the list:
+    Pick a random wall from the list. If only one of the two cells that the wall divides is visited, then:
+    Make the wall a passage and mark the unvisited cell as part of the maze
+    Add the neighboring walls of the cell to the wall list.
+    Remove the wall from the list
+    '''
+    surrounding_cell_count = 0
+    while walls:
+        # Pick a random wall
+        rand_wall = walls[int(random.random()*len(walls))-1]
+
+        # Check if it is a left wall
+        if rand_wall[1] != 0:
+            if maze[rand_wall[0]][rand_wall[1]-1] == open and maze[rand_wall[0]][rand_wall[1]+1] == path:
+
+                surrounding_cell_count = surroundingCells(maze,rand_wall)
+                if surrounding_cell_count < 2:
+
+                    maze[rand_wall[0]][rand_wall[1]] = path
+
+                    # Make the walls
+                    # The top wall
+                    if rand_wall[0] != 0:
+                        if maze[rand_wall[0]-1][rand_wall[1]] != path:
+                            maze[rand_wall[0]-1][rand_wall[1]] = wall
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+                    # Bottom wall
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != path):
+                            maze[rand_wall[0]+1][rand_wall[1]] = wall
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+
+                    # left wall
+                    if (rand_wall[1] != 0):	
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != path):
+                            maze[rand_wall[0]][rand_wall[1]-1] = wall
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+
+                # Delete wall
+                for single_wall in walls:
+                    if (single_wall[0] == rand_wall[0] and single_wall[1] == rand_wall[1]):
+                        walls.remove(single_wall)
+                continue
+
+        # Check if it is a top wall
+        if rand_wall[0] != 0:
+            if maze[rand_wall[0]-1][rand_wall[1]] == open and maze[rand_wall[0]+1][rand_wall[1]] == path:
+
+                surrounding_cell_count = surroundingCells(maze,rand_wall)
+                if surrounding_cell_count < 2:
+
+                    maze[rand_wall[0]][rand_wall[1]] = path
+
+                    # Make the walls
+                    # top wall
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != path):
+                            maze[rand_wall[0]-1][rand_wall[1]] = wall
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+				    # left wall
+                    if (rand_wall[1] != 0):
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != path):
+                            maze[rand_wall[0]][rand_wall[1]-1] = wall
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+
+				    # right wall
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != path):
+                            maze[rand_wall[0]][rand_wall[1]+1] = wall
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+
+                # Delete wall
+                for single_wall in walls:
+                    if (single_wall[0] == rand_wall[0] and single_wall[1] == rand_wall[1]):
+                        walls.remove(single_wall)
+                continue
+
+        # Check if it is a bottom wall
+        if rand_wall[0] != height -1:
+            if maze[rand_wall[0]+1][rand_wall[1]] == open and maze[rand_wall[0]-1][rand_wall[1]] == path:
+
+                surrounding_cell_count = surroundingCells(maze,rand_wall)
+                if surrounding_cell_count < 2:
+
+                    maze[rand_wall[0]][rand_wall[1]] = path
+
+                    # Make the walls
+                    # bottom wall
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != path):
+                            maze[rand_wall[0]+1][rand_wall[1]] = wall
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+
+                    # left wall
+                    if (rand_wall[1] != 0):
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != path):
+                            maze[rand_wall[0]][rand_wall[1]-1] = wall
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+
+                    # right wall
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != path):
+                            maze[rand_wall[0]][rand_wall[1]+1] = wall
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+
+                # Delete wall
+                for single_wall in walls:
+                    if (single_wall[0] == rand_wall[0] and single_wall[1] == rand_wall[1]):
+                        walls.remove(single_wall)
+                continue
+
+
+        # Check if it is a right wall
+        if rand_wall[1] != width-1:
+            if maze[rand_wall[0]][rand_wall[1]+1] == open and maze[rand_wall[0]][rand_wall[1]-1] == path:
+
+                surrounding_cell_count = surroundingCells(maze,rand_wall)
+                if surrounding_cell_count < 2:
+
+                    maze[rand_wall[0]][rand_wall[1]] = path
+
+                    # Make the walls
+                    # right wall
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != path):
+                            maze[rand_wall[0]][rand_wall[1]+1] = wall
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+
+                    # bottom wall
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != path):
+                            maze[rand_wall[0]+1][rand_wall[1]] = wall
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+
+                    # top wall
+                    if rand_wall[0] != 0:	
+                        if maze[rand_wall[0]-1][rand_wall[1]] != path:
+                            maze[rand_wall[0]-1][rand_wall[1]] = wall
+                        if [rand_wall[0]-1, rand_wall[1]] not in walls:
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+        # Delete wall
+        for single_wall in walls:
+            if (single_wall[0] == rand_wall[0] and single_wall[1] == rand_wall[1]):
+                walls.remove(single_wall)
+        continue
+
+
+def surroundingCells(maze,rand_wall):
+    '''
+    get all paths surrounding the wall
+    '''
+    s_cells = 0
+    if maze[rand_wall[0]-1][rand_wall[1]] == path:
+        s_cells += 1
+    if maze[rand_wall[0]+1][rand_wall[1]] == path:
+        s_cells += 1
+    if maze[rand_wall[0]][rand_wall[1]-1] == path:
+        s_cells +=1
+    if maze[rand_wall[0]][rand_wall[1]+1] == path:
+        s_cells += 1
+    return s_cells
 
 def show_menu():
     print("--- Menu ---")
