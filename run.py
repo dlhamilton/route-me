4416dfe6ae65fda35553936015b9aa304ed76dd4
Dan Hamilton <hamiltondan93@gmail.com>
2022-10-06 16:34:08 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def show_menu():
+    print("--- Menu ---")
+    print("1) Create random maze")
+    print("2) Solve Maze")
+    print("3) Create custom maze")
+    print("4) Load maze from file")
+    print("5) Save maze to file")
+    print("6) Exit")
+
+
+def main():
+    print("----------")
+    print("Route Me")
+    print("----------")
+    print("Welcome to Route me the best way to find the quickest route.\n")
+    show_menu()
+
+
+main()
